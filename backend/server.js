a8078489fbb49c4e9bcc1084e66ebbfe7eb23257
Gio Alexander P. Petalcurin <xanderpetalcurin@gmail.com>
2025-03-18 22:39:52 +0800

    Changed DIrectory
    Added Frontend and Backend folders

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 });
 
 // Serve static files from the parent directory
-app.use(express.static(path.join(__dirname, '..')));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Start the server
 app.listen(port, () => {
